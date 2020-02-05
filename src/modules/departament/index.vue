c11d443ca48ab2e53dd49b1a482c2f213060ac3c
Vitor Meira Gomes <vitorg.dev@gmail.com>
2020-02-05 03:23:06 -0300

    Create a feature with filters for departaments

--- src/modules/departament/index.vue
+++ src/modules/departament/index.vue
@@ -1,22 +1,57 @@
 <template>
-  <Card>
-    <template slot="title">Departament View</template>
-    <template slot="text">
-      <b-button @click="create" size="sm" variant="outline-add" class="float-right">+</b-button>
-      <div>
-        <Table
-          @remove="remove"
-          @changeStatus="changeStatus"
-          :key="componentKey"
-          :fields="fields"
-          :items="departaments"
-        ></Table>
-      </div>
-      <router-view></router-view>
-    </template>
-  </Card>
+  <div>
+    <Card>
+      <template slot="title">Filters</template>
+      <template slot="text">
+        <b-form @submit="onSubmit">
+          <b-row>
+            <b-col md="5">
+              <label for="inline-form-input-name">Nome:</label>
+              <b-form-input
+                id="inline-form-input-name"
+                v-model="filters.name"
+                placeholder="Enter your name"
+              ></b-form-input>
+            </b-col>
+            <b-col md="5">
+              <label for="inline-form-input-name">Initials:</label>
+              <b-form-input
+                id="inline-form-input-name"
+                v-model="filters.initials"
+                placeholder="Enter your name"
+              ></b-form-input>
+            </b-col>
+            <b-col md="2">
+              <label for="inline-form-input-name">Status:</label>
+              <b-form-checkbox size="lg" v-model="filters.status" switch></b-form-checkbox>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <b-button type="submit" class="mt-3">Search</b-button>
+            </b-col>
+          </b-row>
+        </b-form>
+      </template>
+    </Card>
+    <Card>
+      <template slot="title">Departament View</template>
+      <template slot="text">
+        <b-button @click="create" size="sm" variant="outline-add" class="float-right">+</b-button>
+        <div>
+          <Table
+            @remove="remove"
+            @changeStatus="changeStatus"
+            :key="componentKey"
+            :fields="fields"
+            :items="departaments"
+          ></Table>
+        </div>
+        <router-view></router-view>
+      </template>
+    </Card>
+  </div>
 </template>
-
 <script>
 import { mapGetters } from "vuex";
 import store from "./_store";
@@ -28,6 +63,11 @@ export default {
   computed: { ...mapGetters({ departaments: "$_departaments/departaments" }) },
   data: function() {
     return {
+      filters: {
+        name: "",
+        initials: "",
+        status: true
+      },
       componentKey: 0,
       fields: [
         { key: "status", label: "Status" },
@@ -55,6 +95,17 @@ export default {
   },
 
   methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      let filledFilters = {};
+      for (const key in this.filters) {
+        if (this.filters.hasOwnProperty(key) && this.filters[key] !== "") {
+          filledFilters[key] = this.filters[key];
+        }
+      }
+      this.$router.push({ name: "departament", query: filledFilters });
+      this.$store.dispatch("$_departaments/list", filledFilters);
+    },
     forceRerender() {
       this.componentKey += 1;
     },
@@ -83,6 +134,12 @@ export default {
   watch: {
     $route() {
       this.forceRerender();
+    },
+    filters: {
+      deep: true,
+      handler() {
+        this.filters.initials = this.filters.initials.toUpperCase();
+      }
     }
   }
 };
